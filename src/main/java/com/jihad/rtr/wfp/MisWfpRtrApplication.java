38c413701beefde9144d16e05ce239d3e03a34a9
jihaddeveloper <jihad.developer@gmail.com>
2021-11-21 15:29:25 +0600

    first commit

---
+++ src/main/java/com/jihad/rtr/wfp/MisWfpRtrApplication.java
@@ -0,0 +1,13 @@
+package com.jihad.rtr.wfp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisWfpRtrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisWfpRtrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jihad/rtr/wfp/MisWfpRtrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jihad.rtr.wfp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisWfpRtrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
